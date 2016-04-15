108e7c785bd8a93dfaa0a05c6d49138cf0969ce3
Lisa Irwin <lirwin@ciena.com>
2016-04-15 11:17:03 -0700

    beginning of star-rated helper

---
+++ app/helpers/star-rated.js
@@ -0,0 +1,7 @@
+import Ember from 'ember'
+
+export function starRated ([starNum, rating]) {
+  return
+}
+
+export default Ember.Helper.helper(starRated)
---
+++ tests/unit/helpers/star-rated-test.js
@@ -0,0 +1,10 @@
+import { starRated } from 'ember-ratings/helpers/star-rated';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | star rated');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = starRated([42]);
+  assert.ok(result);
+});
